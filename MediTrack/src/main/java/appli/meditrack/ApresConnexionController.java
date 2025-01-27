95e6a1dcba5f3e3add631807ef599ecfe9aa8905
IRotcivI <148015994+IRotcivI@users.noreply.github.com>
2025-01-27 15:01:25 +0100

    - Ajout du menu

---
+++ MediTrack/src/main/java/appli/meditrack/ApresConnexionController.java
@@ -0,0 +1,77 @@
+package appli.meditrack;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+
+public class ApresConnexionController {
+
+    @FXML
+    private StackPane contenuDynamique;
+
+    @FXML
+    void deconnexion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirAPropos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirCreationPatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirDemandes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirGestionDossiers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirGestionProduits(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirLiberationChambre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirListeHospitalisations(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirListeProduits(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirManuel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirReapprovisionnement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ouvrirRecherchePatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quitter(ActionEvent event) {
+
+    }
+
+}
