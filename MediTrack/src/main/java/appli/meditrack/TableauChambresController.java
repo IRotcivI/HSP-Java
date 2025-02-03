d18f68c3fc3761e14d2899e94bd23c1b7b89f20d
ASAD_Ra <r.asad@lprs.fr>
2025-02-03 14:56:17 +0100

    Maj

--- MediTrack/src/main/java/appli/meditrack/TableauChambresController.java
+++ MediTrack/src/main/java/appli/meditrack/TableauChambresController.java
@@ -1,5 +1,6 @@
 package appli.meditrack;
 
+import javafx.beans.binding.ObjectExpression;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -21,13 +22,18 @@ public class TableauChambresController {
     @FXML
     private TableColumn<Chambre, Boolean> disponibleCol;
 
+    private static ObjectExpression<Boolean> call(TableColumn.CellDataFeatures<Chambre, Integer> cellData) {
+        return cellData.getValue().numero().asObject();
+    }
+
     @FXML
     public void initialize() {
-
-        numeroCol.setCellValueFactory(cellData -> cellData.getValue().numeroProperty().asObject());
-        typeCol.setCellValueFactory(cellData -> cellData.getValue().typeProperty());
-        disponibleCol.setCellValueFactory(cellData -> cellData.getValue().disponibleProperty().asObject());
-
+        numeroCol.setCellValueFactory(cellData1 -> {
+            ObjectExpression<Boolean> call = call(cellData1);
+            return call;
+        });
+        typeCol.setCellValueFactory(cellData -> cellData.getValue().type());
+        disponibleCol.setCellValueFactory(cellData -> cellData.getValue().disponible().asObject());
 
         ObservableList<Chambre> chambres = FXCollections.observableArrayList(
                 new Chambre(101, "Simple", true),
@@ -35,7 +41,6 @@ public class TableauChambresController {
                 new Chambre(103, "Simple", true),
                 new Chambre(104, "Simple", true)
         );
-
 
         chambreTable.setItems(chambres);
     }
