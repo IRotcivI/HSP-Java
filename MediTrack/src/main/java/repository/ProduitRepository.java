c4e1f6f57b16aee75a790238eb640d1067801eb7
Mp5camon <mp5camon@gmail.com>
2025-02-04 14:30:54 +0100

    ProduitRepository

---
+++ MediTrack/src/main/java/repository/ProduitRepository.java
@@ -0,0 +1,70 @@
+package repository;
+
+import database.Database;
+import model.Produit;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProduitRepository {
+
+    public static void ajouterProduit(Produit produit) {
+        String sql = "INSERT INTO produit (libelle, description, niveauDangerosité) VALUES (?, ?, ?)";
+
+        Database database = new Database(); // Création de l'objet Database
+        Connection connection = database.getConnection(); // Récupération de la connexion
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) { // Préparation de la requête
+            stmt.setString(1, produit.getLibelle());
+            stmt.setString(2, produit.getDescription());
+            stmt.setString(3, produit.getNiveauDangerosite());
+
+            stmt.executeUpdate(); // Exécution de la requête
+            System.out.println("Produit ajouté !");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Produit> getProduits() {
+        List<Produit> produits = new ArrayList<>();
+        String sql = "SELECT * FROM produit";
+
+        Database database = new Database();
+        Connection connection = database.getConnection();
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) { // Exécute la requête
+
+            while (rs.next()) { // Parcours des résultats
+                Produit produit = new Produit(
+                        rs.getString("libelle"),
+                        rs.getString("description"),
+                        rs.getString("niveauDangerosité")
+                );
+                produits.add(produit);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return produits;
+    }
+    public static void supprimerProduit(int id) {
+        String sql = "DELETE FROM produit WHERE id_produit = ?";
+
+        Database database = new Database();
+        Connection connection = database.getConnection();
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            System.out.println("Produit supprimé !");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
