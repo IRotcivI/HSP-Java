ee8a6cf0efdd40c1fead465c908757d19878d849
ahad93140 <r.asad@lprs.fr>
2025-03-20 12:30:34 +0100

    ajout d'une methode ajouterchambre pour connecter avec la bdd

--- MediTrack/src/main/java/database/Database.java
+++ MediTrack/src/main/java/database/Database.java
@@ -1,8 +1,12 @@
 package database;
 
+import model.Chambre;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
 
 public class Database {
     String server ="localhost";
@@ -24,6 +28,25 @@ public class Database {
             System.out.println("Erreur lors de la tentative de connexion à la base de données.");
             e.printStackTrace();
             return null;
+        }
+    }
+    public boolean ajouterChambre(Chambre chambre) {
+        String sql = "INSERT INTO chambre (numero, type, disponible) VALUES (?, ?, ?)";
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+
+
+            statement.setInt(2, chambre.getNumero());
+            statement.setString(1, chambre.getType());
+            statement.setBoolean(3, chambre.isDisponible());
+
+
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
