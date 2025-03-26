896e806e602c1450a7653f16d7cbf7a8f5ef1e5b
ahad93140 <r.asad@lprs.fr>
2025-03-26 16:16:26 +0100

    Correction des erreurs

--- MediTrack/src/main/java/repository/ChambreRepository.java
+++ MediTrack/src/main/java/repository/ChambreRepository.java
@@ -8,45 +8,38 @@ import java.util.ArrayList;
 public class ChambreRepository {
 
 
-    public boolean ajouterChambre(Chambre chambre) {
-        String sql = "INSERT INTO chambre (numero, type, disponible) VALUES (?, ?, ?)";
-        try (Connection connection = getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+    public static boolean ajouterChambre(int numero, String type, String disponible) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/hsp_java", "root", ""
+            );
 
 
+            String sql = "INSERT INTO chambre (numero, type, disponible) VALUES (?, ?, ?)";
+            statement = connection.prepareStatement(sql);
 
-            statement.setInt(2, chambre.getNumero());
-            statement.setString(1, chambre.getType());
-            statement.setBoolean(3, chambre.isDisponible());
+
+            statement.setInt(1, numero);
+            statement.setString(2, type);
+            statement.setString(3, disponible);
 
 
             int rowsInserted = statement.executeUpdate();
             return rowsInserted > 0;
+
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
-        }
-    }
-    public ArrayList<Chambre> getChambres() {
-        ArrayList<Chambre> chambres = new ArrayList<>();
-        String query = "SELECT * FROM chambres";
-
-        try (Connection connection = getConnection();
-             Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery(query)) {
-
-            while (rs.next()) {
-                String type = rs.getString("type");
-                int numero = rs.getInt("numero");
-                boolean disponible = rs.getBoolean("disponible");
-
-                chambres.add(new Chambre(type, numero, disponible));
+        } finally {
+            if (statement != null) {
+                statement.close();
             }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+            if (connection != null) {
+                connection.close();
+            }
         }
-
-        return chambres;
-
     }}
